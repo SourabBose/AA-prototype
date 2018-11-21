348538e359ef75099e14f3f361a4bfa1e274b6ff
SourabBose <lifeofmantra@gmail.com>
2018-11-21 21:10:23 +0530

    add footer

--- js/layout.js
+++ js/layout.js
@@ -1,9 +1,18 @@
+
+
 let sideMenuOpen = false;
 
 document.addEventListener('DOMContentLoaded',function(){
+                //*****************Top Navigation Hamburger Menu Stuff***********//
     document.querySelector('.top-nav-toggle-menu-icon').addEventListener('click',function(e){
         sideMenuOpen = !sideMenuOpen;
         // console.log(sideMenuOpen);
         document.querySelector('.side-nav-container').classList.toggle('side-nav-container-open');
     });
+
+         //***********Setting the current year in copyright text in footer*******//
+    document.querySelector('.footer-copyright-text')
+            .innerHTML = `Copyright &copy ${new Date().getFullYear()} Ancient Astrology. All rights reserved.`;
 });
+
+
