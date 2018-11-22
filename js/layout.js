e9a7e0788c549d6d03be5e59cbb663071606a7a6
SourabBose <lifeofmantra@gmail.com>
2018-11-22 20:27:01 +0530

    remove side menu when backdrop is click

--- js/layout.js
+++ js/layout.js
@@ -1,22 +1,32 @@
-
 // let isLargeScreen = (screen.width >= 1024)? true : false;
 let sideMenuOpen = false; //keeps track of whether currently the side menu is open or closed
                           // side menu should be close by default for smaller screens;
 
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function () {
 
-                //*****************Top Navigation Hamburger Menu Stuff***********//
-    document.querySelector('.top-nav-toggle-menu-icon').addEventListener('click',function(e){
-        sideMenuOpen = !sideMenuOpen; //toggle the current state
-        //display/un-display the side NavBar
-        document.querySelector('.side-nav-container').classList.toggle('side-nav-container-open');
-        //display/un-display the backdrop on top of everything except side and top NavBar
-        document.querySelector('.backdrop').classList.toggle('show-backdrop');
+    let backdrop = document.querySelector('.backdrop');
+    let menuToggleButton = document.querySelector('.top-nav-toggle-menu-icon');
+    let sideNavBar = document.querySelector('.side-nav-container');
+    let footerText = document.querySelector('.footer-copyright-text');
+
+    //remove side menu as well as the backdrop itself when the backdrop is clicked
+    backdrop.addEventListener('click', function (e) {
+        sideNavBar.classList.remove('side-nav-container-open');
+        backdrop.classList.remove('show-backdrop');
     });
 
-         //***********Setting the current year in copyright text in footer*******//
-    document.querySelector('.footer-copyright-text')
-            .innerHTML = `Copyright &copy ${new Date().getFullYear()} Ancient Astrology. All rights reserved.`;
+    //toggle the side menu and backdrop when hamburger menu is clicked
+    menuToggleButton.addEventListener('click', function (e) {
+        sideMenuOpen = !sideMenuOpen; //toggle the var that keeps track of current state of side menu
+        //display/un-display the side NavBar
+        sideNavBar.classList.toggle('side-nav-container-open');
+        //display/un-display the backdrop on top of everything except side and top NavBar
+        backdrop.classList.toggle('show-backdrop');
+    });
+
+    //***********Setting the current year in copyright text in footer*******//
+
+    footerText.innerHTML = `Copyright &copy ${new Date().getFullYear()} Ancient Astrology. All rights reserved.`;
 });
 
 
