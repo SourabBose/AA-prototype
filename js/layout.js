37817a6b261edf8db8b6b2adb95182d4c19ab970
SourabBose <lifeofmantra@gmail.com>
2018-11-22 20:01:49 +0530

    add backdrop when side menu is open

--- js/layout.js
+++ js/layout.js
@@ -1,13 +1,17 @@
 
-
-let sideMenuOpen = false;
+// let isLargeScreen = (screen.width >= 1024)? true : false;
+let sideMenuOpen = false; //keeps track of whether currently the side menu is open or closed
+                          // side menu should be close by default for smaller screens;
 
 document.addEventListener('DOMContentLoaded',function(){
+
                 //*****************Top Navigation Hamburger Menu Stuff***********//
     document.querySelector('.top-nav-toggle-menu-icon').addEventListener('click',function(e){
-        sideMenuOpen = !sideMenuOpen;
-        // console.log(sideMenuOpen);
+        sideMenuOpen = !sideMenuOpen; //toggle the current state
+        //display/un-display the side NavBar
         document.querySelector('.side-nav-container').classList.toggle('side-nav-container-open');
+        //display/un-display the backdrop on top of everything except side and top NavBar
+        document.querySelector('.backdrop').classList.toggle('show-backdrop');
     });
 
          //***********Setting the current year in copyright text in footer*******//
