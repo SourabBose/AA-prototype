3d3124cc145b1452e31240bdca59497db965edcc
SourabBose <lifeofmantra@gmail.com>
2018-11-23 11:43:23 +0530

    finish basic layout for both mobile and desktop

--- js/layout.js
+++ js/layout.js
@@ -1,6 +1,7 @@
-// let isLargeScreen = (screen.width >= 1024)? true : false;
-let sideMenuOpen = false; //keeps track of whether currently the side menu is open or closed
-                          // side menu should be close by default for smaller screens;
+let isLargeScreen = screen.width >= 776;
+let sideMenuOpen = isLargeScreen; //keeps track of whether currently the side menu is open or closed
+                          // side menu should be close by default for smaller screens,
+                            //but for larger screens, side menu should be open by default
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -8,20 +9,32 @@ document.addEventListener('DOMContentLoaded', function () {
     let menuToggleButton = document.querySelector('.top-nav-toggle-menu-icon');
     let sideNavBar = document.querySelector('.side-nav-container');
     let footerText = document.querySelector('.footer-copyright-text');
+    // if screen is large then show the side menu and make offset for main content, initially
+    if(isLargeScreen){
+        sideNavBar.classList.add('side-nav-container-open');
+        document.querySelector('.main-container').classList.add('main-container-offset');
 
-    //remove side menu as well as the backdrop itself when the backdrop is clicked
-    backdrop.addEventListener('click', function (e) {
+    }
+
+    //remove side menu as well as the backdrop itself when the backdrop is clicked(only for smaller screens)
+    backdrop.addEventListener('click', function () {
         sideNavBar.classList.remove('side-nav-container-open');
         backdrop.classList.remove('show-backdrop');
     });
 
     //toggle the side menu and backdrop when hamburger menu is clicked
-    menuToggleButton.addEventListener('click', function (e) {
+    menuToggleButton.addEventListener('click', function () {
         sideMenuOpen = !sideMenuOpen; //toggle the var that keeps track of current state of side menu
         //display/un-display the side NavBar
         sideNavBar.classList.toggle('side-nav-container-open');
-        //display/un-display the backdrop on top of everything except side and top NavBar
-        backdrop.classList.toggle('show-backdrop');
+        if(isLargeScreen){
+            //make/un-make offset for main-content due to side menu appearing(for larger screens)
+            document.querySelector('.main-container').classList.toggle('main-container-offset');
+        }else{
+            //display/un-display the backdrop on top of everything except side and top NavBar(for smaller screens)
+            backdrop.classList.toggle('show-backdrop');
+        }
+
     });
 
     //***********Setting the current year in copyright text in footer*******//
