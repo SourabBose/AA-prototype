317f175651721ba7ed6966cb0c7ec70844c793d8
SourabBose <lifeofmantra@gmail.com>
2018-11-23 17:51:09 +0530

    add non-menu-wrapper and placed main & footer container inside it

--- js/layout.js
+++ js/layout.js
@@ -9,11 +9,11 @@ document.addEventListener('DOMContentLoaded', function () {
     let menuToggleButton = document.querySelector('.top-nav-toggle-menu-icon');
     let sideNavBar = document.querySelector('.side-nav-container');
     let footerText = document.querySelector('.footer-copyright-text');
-    // if screen is large then show the side menu and make offset for main content and footer, initially
+    // if screen is large then show the side menu and make offset for main content and footer(residing inside
+    //          the non-menu-wrapper), initially
     if(isLargeScreen){
         sideNavBar.classList.add('side-nav-container-open');
-        document.querySelector('.main-container').classList.add('main-container-offset');
-        document.querySelector('.footer-container').classList.add('footer-container-offset');
+        document.querySelector('.non-menu-wrapper').classList.add('non-menu-wrapper-offset');
     }
 
     //remove side menu as well as the backdrop itself when the backdrop is clicked(only for smaller screens)
@@ -28,9 +28,10 @@ document.addEventListener('DOMContentLoaded', function () {
         //display/un-display the side NavBar
         sideNavBar.classList.toggle('side-nav-container-open');
         if(isLargeScreen){
-            //make/un-make offset for main-content and footer due to side menu appearing(for larger screens)
-            document.querySelector('.main-container').classList.toggle('main-container-offset');
-            document.querySelector('.footer-container').classList.toggle('footer-container-offset');
+            //make/un-make offset for main-content and footer(both placed inside non-menu-wrapper)
+            // due to side menu appearing(for larger screens)
+            document.querySelector('.non-menu-wrapper').classList.toggle('non-menu-wrapper-offset');
+
         }else{
             //display/un-display the backdrop on top of everything except side and top NavBar(for smaller screens)
             backdrop.classList.toggle('show-backdrop');
